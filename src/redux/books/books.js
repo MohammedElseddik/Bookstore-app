6b6a92079aa1494462f1ffe1ee71cc4da09f2bdf
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-15 22:20:43 +0100

    Define add book to the API functionality

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,16 @@
+import axios from 'axios';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS';
-const beaseUrl =
-  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qLsFjmbcj4drPI4jG9EK/';
+const beaseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qLsFjmbcj4drPI4jG9EK/';
 
-const initailState = [
-  {
-    id: 'book1',
-    title: 'The Redux Books',
-    author: 'Suzanne Collins',
-    category: 'Action',
-  },
-
-  {
-    id: 'book2',
-    title: 'The React Books',
-    author: 'Frank Herbert',
-    category: 'Science Fiction',
-  },
-];
+const initailState = [];
 
 export const addBookAction = (book) => ({
   type: ADD_BOOK,
   payload: {
-    id: book.id,
+    item_id: book.id,
     title: book.title,
     author: book.author,
     category: book.category,
@@ -33,7 +20,7 @@ export const addBookAction = (book) => ({
 export const removBookAction = (id) => ({
   type: REMOVE_BOOK,
   payload: {
-    id,
+    item_id: id,
   },
 });
 
@@ -45,11 +32,20 @@ export const onSuccessAction = (books) => ({
 export const fetchBookApiAction = () => async (dispatch) => {
   const response = await axios.get(`${beaseUrl}books`);
   const booksFetched = Object.entries(response.data).map((item) => {
-    console.log(item);
     const { title, author } = item[1][0];
     return { item_id: item[0], title, author };
   });
   dispatch(onSuccessAction(booksFetched));
+};
+
+export const addBookApi = (book) => async (dispatch) => {
+  console.log(book);
+  await axios.post(`${beaseUrl}books`, book, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  dispatch(addBookAction(book));
 };
 
 const bookReducer = (state = initailState, action) => {
@@ -68,12 +64,12 @@ const bookReducer = (state = initailState, action) => {
       return books;
 
     case REMOVE_BOOK:
-      books = state.filter((book) => book.id !== action.payload.id);
+      books = state.filter((book) => book.item_id !== action.payload.item_id);
       return books;
 
     case FETCH_BOOKS:
-      initailState = action.payload;
-      return initailState;
+      books = action.payload;
+      return books;
 
     default:
       return state;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import bookReducer from './books/book';
+import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
 const rootReducer = combineReducers({
